61ad30f5112b5faa8703de046a5ea793da3cf9e2
bjsdfz17ban <wchengk09@163.com>
2023-05-13 14:47:02 +0800

    changed some code

--- markdown-auto-render.js
+++ markdown-auto-render.js
@@ -2,3 +2,12 @@ var DOMs = document.querySelectorAll('md');
 var conv = new showdown.Converter();
 for (var i = 0;i < DOMs.length;i ++)
     DOMs[i].innerHTML = conv.makeHtml(DOMs[i].innerHTML);
+renderMathInElement(document.getElementById("display"), {
+        delimiters: [
+        {left: '$$', right: '$$', display: true},
+        {left: '$', right: '$', display: true},
+        {left: '\\(', right: '\\)', display: true},
+        {left: '\\[', right: '\\]', display: true}
+        ],
+        // • rendering keys, e.g.:
+        throwOnError : false
