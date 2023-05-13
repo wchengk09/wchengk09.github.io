eff413f59249624f0637c780f543a67e146ab0b1
bjsdfz17ban <wchengk09@163.com>
2023-05-13 14:45:01 +0800

    changed some code

--- markdown-auto-render.js
+++ markdown-auto-render.js
@@ -1,4 +1,4 @@
 var DOMs = document.querySelectorAll('md');
 var conv = new showdown.Converter();
 for (var i = 0;i < DOMs.length;i ++)
-    DOMs[i].innerHTML = conv.makeHTML
+    DOMs[i].innerHTML = conv.makeHtml(DOMs[i].innerHTML);
