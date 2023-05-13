b0b9c7e5555842bb908982e83d9402907ddc7f64
bjsdfz17ban <wchengk09@163.com>
2023-05-13 12:02:01 +0800

    changed some code

--- showdown/src/subParsers/makemarkdown/pre.js
+++
@@ -1,6 +0,0 @@
-showdown.subParser('makeMarkdown.pre', function (node, options, globals) {
-  'use strict';
-
-  var num  = node.getAttribute('prenum');
-  return '<pre>' + globals.preList[num] + '</pre>';
-});
