c1569bc0733c0f46bfd4d99ff0bec0b5d4368669
bjsdfz17ban <wchengk09@163.com>
2023-05-14 09:56:02 +0800

    changed some code

--- markdown-auto-render.js
+++ markdown-auto-render.js
@@ -2,7 +2,7 @@ var DOMs = document.querySelectorAll('md');
 var conv = new showdown.Converter();
 for (var i = 0;i < DOMs.length;i ++)
     DOMs[i].innerHTML = conv.makeHtml(DOMs[i].innerHTML);
-/*renderMathInElement(document.body, {
+renderMathInElement(document.body, {
     delimiters: [
         {left: '$$', right: '$$', display: true},
         {left: '$', right: '$', display: false},
@@ -10,4 +10,4 @@ for (var i = 0;i < DOMs.length;i ++)
         {left: '\\[', right: '\\]', display: true}
     ],
     throwOnError : false
-});*/
+});
