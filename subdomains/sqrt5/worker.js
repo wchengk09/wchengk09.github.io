d860e0b910b2632660fbfd9a342f100cfdc280a8
wchengk09 <wchengk09@163.com>
2024-05-29 21:15:57 +0800

    modified:   subdomains/sqrt5/index.html 	modified:   subdomains/sqrt5/worker.js

--- subdomains/sqrt5/worker.js
+++ subdomains/sqrt5/worker.js
@@ -1,2 +1,13 @@
 // calculate in the backend
-var 
+var a = 1n,b = 5n;
+function calc(){
+    a = (a + b / a) / 2n;
+    a *= 10n;
+    b *= 100n;
+}
+setInterval(calc,0);
+
+this.addEventListener('message',function(e){
+    // console.log('114');
+    this.postMessage(a);
+})
